7462b1cc9a46f694d01933c93676156fa186ebf2
soonpyo21 <tnsvy2004@gmail.com>
2023-01-05 15:59:00 +0900

    Update Main.java
    그리디 알고리즘 - 곱하기 혹은 더하기

--- src/Main.java
+++ src/Main.java
@@ -2,44 +2,44 @@ import java.io.*;
 import java.util.Scanner;
 
 // 문제
-// 어떠한 수 N이 1이 될 때까지 다음의 두 과정 중 하나를 반복적으로 선택하여 수행하려 한다. 단, 두번째 연산은 N이 K로 나누어 떨어질 때만 선택할 수 있다.
-// 1. N에서 1을 뺀다.
-// 2. N을 K로 나눈다.
-// 예를 들어 N이 17, K가 4라고 가정한다. 이떄 1번의 과정을 한번 수행하면 N은 16이 된다. 이후에 2번의 과정을 두 번 수행하면 N은 1이 된다.
-// 결과적으로 이 경우 전체 과정을 실행한 횟수는 3이 된다. 이는 N을 1로 만드는 최소 횟수이다.
-// N과 K가 주어질 때 N이 1이 될 때까지 1번 혹은 2번의 과정을 수행해야 하는 최소 횟수를 구하는 프로그램을 작성하시오.
+// 각 자리가 숫자로만 이루어진 문자열 S가 주어졌을 때, 
+// 왼쪽부터 오른쪽으로 하나씩 모든 숫자를 확인하며 숫자 사이에 'x' 혹은 '+' 연산자를 넣어 
+// 결과적으로 만들어질 수 있는 가장 큰 수를 구하는 프로그램을 작성하세요.
+// 단, +보다 x를 먼저 계산하는 일반적인 방식과는 달리, 모든 연산은 왼쪽에서부터 순서대로 이루어진다고 가정합니다.
+//
+// 예를 들어 02984라는 문자열로 만들 수 있는 가장 큰 수는 ((((0 + 2) x 9) x 8) x 4) = 576입니다. 
+// 또한 만들어질 수 있는 가장 큰 수는 항상 20억 이하의 정수가 되도록 입력이 주어집니다.
 //
 // 입력
-// 첫째 줄에 (N<1<=N<=100,000)과 K(2<=K<=100,000)가 공백을 기준으로 하여 각각 자연수로 주어진다.
+// 첫째 줄에 여러개의 숫자로 구성된 하나의 문자열 S가 주어집니다. (1 <= S의 길이 <= 20)
 //
 // 출력
-// 첫째 줄에 N이 1이 될 때까지 1번 혹은 2번의 과정을 수행해야 하는 횟수의 최솟값을 출력한다.
+// 첫째 줄에 만들어 질 수 있는 가장 큰 수를 출력합니다.
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
-
-        // N, K를 공백을 기준으로 구분하여 입력 받기
-        int N = sc.nextInt();
-        int K = sc.nextInt();
-        int result = 0;
-
-        while(true) {
-            // N이 K로 나누어 떨어지는 수가 될 때까지 빼기
-            int target = (N/K) * K;
-            result += (N - target);
-            N = target;
-            // N이 K보다 작을 때, (더 이상 나눌 수 없을 때) 반복문 탈출
-            if(N < K)  break;
-            // K로 나누기
-            result += 1;
-            N /= K;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] S = br.readLine().split("");
+        int result = Integer.parseInt(S[0]);
+        
+        for(int i = 0; i < S.length; i++) {
+            if(i != S.length-1) {
+                int num = Integer.parseInt(S[i + 1]);
+                
+                if(num == 0 || num == 1 || result == 0) {
+                    result += num;
+                } else {
+                    result *= num;
+                }
+            }
         }
-        // 마지막으로 남은 수에 대하여 1씩 빼기
-        result += (N-1);
-        System.out.println(result);
-
+        
+        bw.write(result + "");
+        br.close();
+        bw.close();
     }
 }
