f379c5b38bb8c32bcc95744b8252736879722b6c
soonpyo21 <tnsvy2004@gmail.com>
2023-01-09 15:16:37 +0900

    그리디 알고리즘 - 상하좌우 (모범답안)

--- src/Main.java
+++ src/Main.java
@@ -25,42 +25,38 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-    
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamReader(System.out));
-        
-        int N = Integer.parseInt(br.readLine());
-        String[] moves = br.readLine().split(" ");
-        
-        int row = 1;
-        int col = 1;
-        
-        for(int i = 0; i < moves.length; i++) {
-            if(moves[i].equals("U")) {
-                if(row - 1 > 0) {
-                    row --;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // N을 입력받기
+        int n = sc.nextInt();
+        sc.nextLine(); // 버퍼 비우기
+        String[] plans = sc.nextLine().split(" ");
+        int x = 1, y = 1;
+
+        // L, R, U, D에 따른 이동 방향 
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {-1, 1, 0, 0};
+        char[] moveTypes = {'L', 'R', 'U', 'D'};
+
+        // 이동 계획을 하나씩 확인
+        for (int i = 0; i < plans.length; i++) {
+            char plan = plans[i].charAt(0);
+            // 이동 후 좌표 구하기 
+            int nx = -1, ny = -1;
+            for (int j = 0; j < 4; j++) {
+                if (plan == moveTypes[j]) {
+                    nx = x + dx[j];
+                    ny = y + dy[j];
                 }
             }
-            else if (moves[i].equals("D")) {
-                if(row + 1 <= N) {
-                    row ++;
-                }
-            }
-            else if(moves[i].equals("R")) {
-                if(col + 1 <= N) {
-                    col ++;
-                }
-            }
-            else if(moves[i].equals("L")) {
-                if(col - 1 > 0) {
-                    col --;
-                }
-            }
+            // 공간을 벗어나는 경우 무시 
+            if (nx < 1 || ny < 1 || nx > n || ny > n) continue;
+            // 이동 수행 
+            x = nx;
+            y = ny;
         }
-        
-        bw.write(row + " " + col);
-        br.close();
-        bw.close();
+
+        System.out.println(x + " " + y);
     }
 }
