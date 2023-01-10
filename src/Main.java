24f297402d339179a1e8af44bd5704c0f3d9d3cd
soonpyo21 <tnsvy2004@gmail.com>
2023-01-10 11:09:35 +0900

    그리디 알고리즘 - 시각 (모범답안)

--- src/Main.java
+++ src/Main.java
@@ -21,26 +21,29 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int N = Integer.parseInt(br.readLine());
+     // 특정한 시각 안에 '3'이 포함되어 있는지의 여부
+    public static boolean check(int h, int m, int s) {
+        if (h % 10 == 3 || m / 10 == 3 || m % 10 == 3 || s / 10 == 3 || s % 10 == 3)
+            return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // H를 입력받기 
+        int h = sc.nextInt();
         int cnt = 0;
-        
-        for(int i = 0; i <= N; i ++) {
-            for(int j = 0; j < 60; j ++) {
-                for(int k = 0; k < 60; k ++) {
-                    if(String.valueOf(i).contains("3") || String.valueOf(j).contains("3") || String.valueOf(k).contains("3")) {
-                        cnt ++;
-                    }
+
+        for (int i = 0; i <= h; i++) {
+            for (int j = 0; j < 60; j++) {
+                for (int k = 0; k < 60; k++) {
+                    // 매 시각 안에 '3'이 포함되어 있다면 카운트 증가
+                    if (check(i, j, k)) cnt++;
                 }
             }
         }
-        
-        bw.write(cnt + "");
-        br.close;
-        bw.close;
+
+        System.out.println(cnt);
     }
 }
