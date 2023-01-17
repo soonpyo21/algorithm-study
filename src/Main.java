453e46f851808e7f3e47ca3c8663a26a0061b1fc
soonpyo21 <tnsvy2004@gmail.com>
2023-01-17 14:53:17 +0900

    그리디 알고리즘 - 왕실의 나이트 (모범답안)

--- src/Main.java
+++ src/Main.java
@@ -21,55 +21,30 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-         
-         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-         
-         String[] input = br.readLine().split("");
-         int row = Integer.parseInt(input[1]);
-         int col = input[0].charAt(0) - 96;
-         int cnt = 0;
-         
-         if(row - 2 > 0) {
-              if(col - 1 > 0) {
-                   cnt ++;
-              }
-              if(col + 1 < 9) {
-                   cnt ++;
-              }
-         }
-         
-         if(row + 2 < 9) {
-              if(col - 1 > 0) {
-                   cnt ++;
-              }
-              if(col + 1 < 9) {
-                   cnt ++;
-              }
-         }
-         
-         if(col - 2 > 0) {
-              if(row - 1 > 0) {
-                   cnt ++;
-              }
-              if(row + 1 < 9) {
-                   cnt ++;
-              }
-         }
-         
-         if(col + 2 < 9) {
-              if(row - 1 > 0) {
-                   cnt ++;
-              }
-              if(row + 1 < 9) {
-                   cnt ++;
-              }
-         }
-         
-         bw.write(cnt + "");
-         br.close();
-         bw.close();
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        // 현재 나이트의 위치 입력받기
+        String inputData = sc.nextLine();
+        int row = inputData.charAt(1) - '0';
+        int column = inputData.charAt(0) - 'a' + 1;
+
+        // 나이트가 이동할 수 있는 8가지 방향 정의
+        int[] dx = {-2, -1, 1, 2, 2, 1, -1, -2};
+        int[] dy = {-1, -2, -2, -1, 1, 2, 2, 1};
+
+        // 8가지 방향에 대하여 각 위치로 이동이 가능한지 확인
+        int result = 0;
+        for (int i = 0; i < 8; i++) {
+            // 이동하고자 하는 위치 확인
+            int nextRow = row + dx[i];
+            int nextColumn = column + dy[i];
+            // 해당 위치로 이동이 가능하다면 카운트 증가
+            if (nextRow >= 1 && nextRow <= 8 && nextColumn >= 1 && nextColumn <= 8) {
+                result += 1;
+            }
+        }
+
+        System.out.println(result);
     }
 }
