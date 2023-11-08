5a6b959c71acdc62327b5d5e1ff2052afe131fd2
soonpyo21 <tnsvy2004@gmail.com>
2023-11-09 00:44:17 +0900

    그리디 알고리즘 - 1이 될 때까지 (모범답안)

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,56 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 // 문제
-// 어떠한 수 N이 1이 될 때까지 다음의 두 과정 중 하나를 반복적으로 선택하여 수행하려 한다. 단, 두번째 연산은 N이 K로 나누어 떨어질 때만 선택할 수 있다.
-// 1. N에서 1을 뺀다.
-// 2. N을 K로 나눈다.
-// 예를 들어 N이 17, K가 4라고 가정한다. 이떄 1번의 과정을 한번 수행하면 N은 16이 된다. 이후에 2번의 과정을 두 번 수행하면 N은 1이 된다.
-// 결과적으로 이 경우 전체 과정을 실행한 횟수는 3이 된다. 이는 N을 1로 만드는 최소 횟수이다.
-// N과 K가 주어질 때 N이 1이 될 때까지 1번 혹은 2번의 과정을 수행해야 하는 최소 횟수를 구하는 프로그램을 작성하시오.
+// 인하은행에는 ATM이 1대밖에 없다. 지금 이 ATM앞에 N명의 사람들이 줄을 서있다.
+// 사람은 1번부터 N번까지 번호가 매겨져 있으며, i번 사람이 돈을 인출하는데 걸리는 시간은 Pi분이다.
 //
-// 입력
-// 첫째 줄에 (N<1<=N<=100,000)과 K(2<=K<=100,000)가 공백을 기준으로 하여 각각 자연수로 주어진다.
+// 사람들이 줄을 서는 순서에 따라서, 돈을 인출하는데 필요한 시간의 합이 달라지게 된다.
+// 예를 들어, 총 5명이 있고, P1 = 3, P2 = 1, P3 = 4, P4 = 3, P5 = 2 인 경우를 생각해보자.
+// [1, 2, 3, 4, 5] 순서로 줄을 선다면, 1번 사람은 3분만에 돈을 뽑을 수 있다.
+// 2번 사람은 1번 사람이 돈을 뽑을 때 까지 기다려야 하기 때문에, 3+1 = 4분이 걸리게 된다.
+// 3번 사람은 1번, 2번 사람이 돈을 뽑을 때까지 기다려야 하기 때문에, 총 3+1+4 = 8분이 필요하게 된다.
+// 4번 사람은 3+1+4+3 = 11분, 5번 사람은 3+1+4+3+2 = 13분이 걸리게 된다.
+// 이 경우에 각 사람이 돈을 인출하는데 필요한 시간의 합은 3+4+8+11+13 = 39분이 된다.
 //
-// 출력
-// 첫째 줄에 N이 1이 될 때까지 1번 혹은 2번의 과정을 수행해야 하는 횟수의 최솟값을 출력한다.
+// 줄을 [2, 5, 1, 4, 3] 순서로 줄을 서면, 2번 사람은 1분만에, 5번 사람은 1+2 = 3분, 1번 사람은 1+2+3 = 6분,
+// 4번 사람은 1+2+3+3 = 9분, 3번 사람은 1+2+3+3+4 = 13분이 걸리게 된다. 각 사람이 돈을 인출하는데 필요한 시간의 합은 1+3+6+9+13 = 32분이다.
+// 이 방법보다 더 필요한 시간의 합을 최소로 만들 수는 없다.
+//
+// 줄을 서 있는 사람의 수 N과 각 사람이 돈을 인출하는데 걸리는 시간 Pi가 주어졌을 때,
+// 각 사람이 돈을 인출하는데 필요한 시간의 합의 최솟값을 구하는 프로그램을 작성하시오.
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // N, K를 공백을 기준으로 구분하여 입력 받기
-        int N = sc.nextInt();
-        int K = sc.nextInt();
-        int result = 0;
+        int N = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+        int[] times = new int[N];
+        int minTime = 0;
 
-        while(true) {
-            // N이 K로 나누어 떨어지는 수가 될 때까지 빼기
-            int target = (N/K) * K;
-            result += (N - target);
-            N = target;
-            // N이 K보다 작을 때, (더 이상 나눌 수 없을 때) 반복문 탈출
-            if(N < K)  break;
-            // K로 나누기
-            result += 1;
-            N /= K;
+        for(int i = 0; i < N; i ++) {
+            times[i] = Integer.parseInt(input[i]);
         }
-        // 마지막으로 남은 수에 대하여 1씩 빼기
-        result += (N-1);
-        System.out.println(result);
 
+        Arrays.sort(times);
+
+        for(int i = 0; i < N; i ++) {
+            minTime += times[i];
+            int num = i;
+            if(num > 0){
+                for(int j = num; j > 0; j --) {
+                    minTime += times[j-1];
+                }
+            }
+        }
+
+        bw.write(minTime + "");
+        br.close();
+        bw.close();
     }
 }
