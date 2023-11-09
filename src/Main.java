ad11570db7a2e2602e7672a74d92bdcd850740d7
soonpyo21 <tnsvy2004@gmail.com>
2023-11-10 00:00:54 +0900

    그리디 알고리즘 - 동전 0

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,15 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 // 문제
-// 인하은행에는 ATM이 1대밖에 없다. 지금 이 ATM앞에 N명의 사람들이 줄을 서있다.
-// 사람은 1번부터 N번까지 번호가 매겨져 있으며, i번 사람이 돈을 인출하는데 걸리는 시간은 Pi분이다.
+// 준규가 가지고 있는 동전은 총 N종류이고, 각각의 동전을 매우 많이 가지고 있다.
+// 동전을 적절히 사용해서 그 가치의 합을 K로 만들려고 한다. 이때 필요한 동전 개수의 최솟값을 구하는 프로그램을 작성하시오.
 //
-// 사람들이 줄을 서는 순서에 따라서, 돈을 인출하는데 필요한 시간의 합이 달라지게 된다.
-// 예를 들어, 총 5명이 있고, P1 = 3, P2 = 1, P3 = 4, P4 = 3, P5 = 2 인 경우를 생각해보자.
-// [1, 2, 3, 4, 5] 순서로 줄을 선다면, 1번 사람은 3분만에 돈을 뽑을 수 있다.
-// 2번 사람은 1번 사람이 돈을 뽑을 때 까지 기다려야 하기 때문에, 3+1 = 4분이 걸리게 된다.
-// 3번 사람은 1번, 2번 사람이 돈을 뽑을 때까지 기다려야 하기 때문에, 총 3+1+4 = 8분이 필요하게 된다.
-// 4번 사람은 3+1+4+3 = 11분, 5번 사람은 3+1+4+3+2 = 13분이 걸리게 된다.
-// 이 경우에 각 사람이 돈을 인출하는데 필요한 시간의 합은 3+4+8+11+13 = 39분이 된다.
+// 입력
+// 첫째 줄에 N과 K가 주어진다. (1 ≤ N ≤ 10, 1 ≤ K ≤ 100,000,000)
+// 둘째 줄부터 N개의 줄에 동전의 가치 Ai가 오름차순으로 주어진다. (1 ≤ Ai ≤ 1,000,000, A1 = 1, i ≥ 2인 경우에 Ai는 Ai-1의 배수)
 //
-// 줄을 [2, 5, 1, 4, 3] 순서로 줄을 서면, 2번 사람은 1분만에, 5번 사람은 1+2 = 3분, 1번 사람은 1+2+3 = 6분,
-// 4번 사람은 1+2+3+3 = 9분, 3번 사람은 1+2+3+3+4 = 13분이 걸리게 된다. 각 사람이 돈을 인출하는데 필요한 시간의 합은 1+3+6+9+13 = 32분이다.
-// 이 방법보다 더 필요한 시간의 합을 최소로 만들 수는 없다.
-//
-// 줄을 서 있는 사람의 수 N과 각 사람이 돈을 인출하는데 걸리는 시간 Pi가 주어졌을 때,123
-// 각 사람이 돈을 인출하는데 필요한 시간의 합의 최솟값을 구하는 프로그램을 작성하시오.
+// 출력
+// 첫째 줄에 K원을 만드는데 필요한 동전 개수의 최솟값을 출력한다.
 
 public class Main {
 
@@ -28,29 +20,33 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        String[] input = br.readLine().split(" ");
-        int[] times = new int[N];
-        int minTime = 0;
+        String input[] = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+        int[] values = new int[N];
+        int cnt = 0;
 
         for(int i = 0; i < N; i ++) {
-            times[i] = Integer.parseInt(input[i]);
+            int value = Integer.parseInt(br.readLine());
+            values[i] = value;
         }
 
-        Arrays.sort(times);
-
-        for(int i = 0; i < N; i ++) {
-            minTime += times[i];
-            int num = i;
-            if(num > 0){
-                for(int j = num; j > 0; j --) {
-                    minTime += times[j-1];
-                }
+        for(int i = N-1; i > -1; i --) {
+            while(K / values[i] > 0) {
+                K -= values[i];
+                cnt++;
             }
         }
 
-        bw.write(minTime + "");
+        bw.write(cnt + "");
         br.close();
         bw.close();
     }
 }
+
+/* 위의 35 ~ 38번 라인은 아래 코드가 더 나은 방향이다. (백준 참조)
+if(K >= values[i]) {
+    cnt += K / values[i];
+    K = K % values[i];
+}
+*/
