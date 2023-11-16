b45b0193c9d8bf32d54f5001b888ee4fdc6fb56e
soonpyo21 <tnsvy2004@gmail.com>
2023-11-17 00:31:04 +0900

    그리디 알고리즘 - 회의실 배정 막힘

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,21 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
-// 문제
-// 준규가 가지고 있는 동전은 총 N종류이고, 각각의 동전을 매우 많이 가지고 있다.
-// 동전을 적절히 사용해서 그 가치의 합을 K로 만들려고 한다. 이때 필요한 동전 개수의 최솟값을 구하는 프로그램을 작성하시오.
-//
-// 입력
-// 첫째 줄에 N과 K가 주어진다. (1 ≤ N ≤ 10, 1 ≤ K ≤ 100,000,000)
-// 둘째 줄부터 N개의 줄에 동전의 가치 Ai가 오름차순으로 주어진다. (1 ≤ Ai ≤ 1,000,000, A1 = 1, i ≥ 2인 경우에 Ai는 Ai-1의 배수)
-//
-// 출력
-// 첫째 줄에 K원을 만드는데 필요한 동전 개수의 최솟값을 출력한다.
+/*
+문제
+한 개의 회의실이 있는데 이를 사용하고자 하는 N개의 회의에 대하여 회의실 사용표를 만들려고 한다.
+각 회의 I에 대해 시작시간과 끝나는 시간이 주어져 있고, 각 회의가 겹치지 않게 하면서 회의실을 사용할 수 있는 회의의 최대 개수를 찾아보자.
+단, 회의는 한번 시작하면 중간에 중단될 수 없으며 한 회의가 끝나는 것과 동시에 다음 회의가 시작될 수 있다.
+회의의 시작시간과 끝나는 시간이 같을 수도 있다. 이 경우에는 시작하자마자 끝나는 것으로 생각하면 된다.
 
+입력
+첫째 줄에 회의의 수 N(1 ≤ N ≤ 100,000)이 주어진다.
+둘째 줄부터 N+1 줄까지 각 회의의 정보가 주어지는데 이것은 공백을 사이에 두고 회의의 시작시간과 끝나는 시간이 주어진다.
+시작 시간과 끝나는 시간은 231-1보다 작거나 같은 자연수 또는 0이다.
+
+출력
+첫째 줄에 최대 사용할 수 있는 회의의 최대 개수를 출력한다.
+*/
 public class Main {
 
     public static void main(String[] args) throws IOException {
@@ -20,22 +24,33 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String input[] = br.readLine().split(" ");
-        int N = Integer.parseInt(input[0]);
-        int K = Integer.parseInt(input[1]);
-        int[] values = new int[N];
-        int cnt = 0;
+        int N = Integer.parseInt(br.readLine());
+        int[] startTime = new int[N];
+        int[] endTime = new int[N];
 
         for(int i = 0; i < N; i ++) {
-            int value = Integer.parseInt(br.readLine());
-            values[i] = value;
+            String[] input = br.readLine().split(" ");
+            startTime[i] = Integer.parseInt(input[0]);
+            endTime[i] = Integer.parseInt(input[1]);
         }
 
-        for(int i = N-1; i > -1; i --) {
-            while(K / values[i] > 0) {
-                K -= values[i];
-                cnt++;
+        int minTime = Arrays.stream(startTime).min().getAsInt();
+        int maxTime = Arrays.stream(endTime).max().getAsInt();
+        int tmpTime = maxTime;
+        int cnt = 0;
+
+        while(tmpTime == maxTime) {
+            for(int i = 0; i < N; i ++) {
+                if(startTime[i] == minTime) {   // 시작 시간이 가장 낮은 것들 중에
+                    if(endTime[i] < tmpTime) {  // 끝나는 시간이 tmpTime보다 작다면
+                        tmpTime = endTime[i];   // 현재 가장 빨리 끝나는 시간이다.
+                    }
+                }
             }
+            cnt++;
+            minTime = tmpTime;
+            System.out.println("]] 1 : " + minTime);
+            System.out.println("]] 2 : " + maxTime);
         }
 
         bw.write(cnt + "");
@@ -43,10 +58,3 @@ public class Main {
         bw.close();
     }
 }
-
-/* 위의 35 ~ 38번 라인은 아래 코드가 더 나은 방향이다. (백준 참조)
-if(K >= values[i]) {
-    cnt += K / values[i];
-    K = K % values[i];
-}
-*/
